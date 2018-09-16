2b0a3addc2e39392f2250e6560a596da2976698c
Sherry Martin <sm674@cornell.edu>
2018-09-16 22:38:41 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -7,3 +7,13 @@ def my_find(collection)
   i += 1
 end
 end
+
+def my_find(collection) 
+  i = 0 
+  while i < collection.length
+  if yield(collection[i]) 
+    return collection[i]
+  end
+  i = i + 1 
+end
+end
